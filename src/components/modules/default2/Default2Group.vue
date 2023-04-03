b9aa538e9da0120b06ac2b74b9a0bca6e30f34e2
david <david@ellis128.co.uk>
2023-04-03 12:46:51 +0100

    added css class

--- src/components/modules/default2/Default2Group.vue
+++ src/components/modules/default2/Default2Group.vue
@@ -1,25 +1,23 @@
 <template>
-  <div class="q-pa-xs row">
-    Group Component
-    <h4>{{ name }}</h4>
-    <div v-for="item in groupItems" :key="item">
-      <h5>{{item.type}}</h5>
+  <div style="border:2px solid grey">
+    <div class = "GroupTitle"> {{ name }}</div>
+    <div class="q-pa-xs row">
+      <div v-for="item in groupItems" :key="item" >
+        <NodeVariable v-if="item.type=='NodeVariable'"
+                      :node-number=store.state.selected_node
+                      :name="item.title"
+                      :node-variable-index=item.nodeVariableIndex>
+        </NodeVariable>
+        <NodeVariableBitSingle v-if="item.type=='NodeVariableBitSingle'"
+                                  :NodeNumber="store.state.selected_node"
+                                  :VariableIndex=item.nodeVariableIndex
+                                  :Title="item.title"
+                                  :Description="item.description"
+                                  :Bit=item.bit
+                                  :Name="item.title">
+        </NodeVariableBitSingle>
 
-       <NodeVariable v-if="item.type=='NodeVariable'"
-                    :node-number=store.state.selected_node
-                    :name="item.title"
-                    :node-variable-index=item.nodeVariableIndex>
-      </NodeVariable>
-      <NodeVariableBitSingle v-if="item.type=='NodeVariableBitSingle'"
-                                :NodeNumber="store.state.selected_node"
-                                :VariableIndex=item.nodeVariableIndex
-                                :Title="item.title"
-                                :Description="item.description"
-                                :Bit=item.bit
-                                :Name="item.title"
-      >
-      </NodeVariableBitSingle>
-
+      </div>
     </div>
 
   </div>
@@ -48,7 +46,8 @@ export default {
       console.log("Group onMounted")
 //      console.log(`Group name : ${this.name}`)
 //      console.log(`Group items : ${JSON.stringify(groupItems)}`)
-      console.log(`Group store : ${JSON.stringify(store.state)}`)
+      console.log(`Group store selected node : ${JSON.stringify(store.state.selected_node)}`)
+      console.log(`Group store NV: ${JSON.stringify(store.state.nodes[store.state.selected_node].variableConfig.nv)}`)
     })
     return {store}
   },
@@ -94,5 +93,7 @@ onMounted(() => {
  -->
 
 <style scoped>
-
+.GroupTitle {
+  text-align: center;
+}
 </style>
