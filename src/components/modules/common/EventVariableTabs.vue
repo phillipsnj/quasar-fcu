51c3c28b7f4382d84d56c694ae78a1d05e43a908
david <david@ellis128.co.uk>
2023-04-26 19:32:59 +0100

    Progress on EventVariableTabs.vue

--- src/components/modules/common/EventVariableTabs.vue
+++ src/components/modules/common/EventVariableTabs.vue
@@ -1,34 +1,29 @@
 <template>
     <div class="q-pa-md">
-      <div class="q-gutter-y-md" style="max-width: 600px">
 
-        <q-tabs v-model="selectedTab">
-          <q-tab v-for="tab in tabPanels" :key="tab.name"
-            :label="tab.name"
-            :name="tab.name"
-          />
-        </q-tabs>
-  
+      <q-tabs v-model="selectedTab">
+        <q-tab v-for="tab in tabPanels" :key="tab.name"
+          :label="tab.name"
+          :name="tab.name"
+        />
+      </q-tabs>
 
-        <q-tab-panels keep-alive v-model="selectedTab">
-          <q-tab-panel v-for="tab in tabPanels" :key="tab.name" :name="tab.name" >
-            <!-- <component :is="tab.componentName"></component> -->
+      <q-tab-panels keep-alive v-model="selectedTab">
+        <q-tab-panel v-for="tab in tabPanels" :key="tab.name" :name="tab.name" >
+          <div v-for="item in tab.items" :key="item">
+            <EventVariableNumber v-if="item.type=='EventVariableNumber'"
+                :node-number=store.state.selected_node
+                :eventIndex = store.state.selected_event_index
+                :eventVariableIndex= "item.eventVariableIndex"
+                :startBit = "item.startBit"
+                :endBit = "item.endBit"
+                :displayOffset = "item.displayOffset"
+                :name="item.displayTitle">
+            </EventVariableNumber>
+          </div>
+        </q-tab-panel>
+      </q-tab-panels>
 
-            <div v-for="item in tab.items" :key="item">
-              <EventVariableNumber v-if="item.type=='EventVariableNumber'"
-                  :node-number=store.state.selected_node
-                  :eventIndex = store.state.selected_event_index
-                  :eventVariableIndex= "item.eventVariableIndex"
-                  :startBit = "item.startBit"
-                  :endBit = "item.endBit"
-                  :displayOffset = "item.displayOffset"
-                  :name="item.displayTitle">
-              </EventVariableNumber>
-            </div>
-          </q-tab-panel>
-        </q-tab-panels>
-
-      </div>
     </div>
   </template>
   
@@ -41,6 +36,7 @@
   import EventVariableNumber from "components/modules/common/EventVariableNumber"
 
   const tabPanels = ref()
+  const selectedTab = ref()
   
   export default {
     props: {
@@ -55,11 +51,12 @@
         console.log('tabs props: ' + JSON.stringify(props))
         tabPanels.value = props.configuration.tabPanels
         console.log('tabs tabPanels: ' + JSON.stringify(tabPanels.value))
+        selectedTab.value = tabPanels.value[0].name
       })
 
       return {
         store,
-        selectedTab: ref('One'),
+        selectedTab,
         tabPanels
       }
     }
